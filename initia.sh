a2e72d50683d4c8d79404e4cbc8c5c1c9d18caa2
smeb y <48400087+a3165458@users.noreply.github.com>
2024-05-15 09:31:10 +0800

    Update initia.sh

--- initia.sh
+++ initia.sh
@@ -111,10 +111,8 @@ function install_node() {
     pm2 stop initiad
     
     # 配置快照
-    wget -O initia_113153.tar.lz4 https://snapshots.polkachu.com/testnet-snapshots/initia/initia_113153.tar.lz4 --inet4-only
-    initiad tendermint unsafe-reset-all --home $HOME/.initia --keep-addr-book
-    lz4 -d -c ./initia_113153.tar.lz4 | tar -xf - -C $HOME/.initia
-    cp ~/.initia/priv_validator_state.json  ~/.initia/data/priv_validator_state.json
+    curl -L https://snapshots.kzvn.xyz/initia/initiation-1_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.initia
+    mv $HOME/.initia/priv_validator_state.json.backup $HOME/.initia/data/priv_validator_state.json
     
     pm2 start ./build/slinky -- --oracle-config-path ./config/core/oracle.json --market-map-endpoint 0.0.0.0:53490
     pm2 restart initiad
